d0ca70492f15a95c01963a2a7bc91c765cbc95d7
Ludvig Viveland <ludvigvive@gmail.com>
2020-12-29 14:33:53 +0100

    small fix

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require("fs");
 var url = require("url");
+var arrayV = ['T', 'Z', 'Y', 'X', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'X'];
 function savePage(res, query) {
     res.write("<!DOCTYPE html>\n");
     res.write("<html>\n");
@@ -8,30 +9,35 @@ function savePage(res, query) {
     res.write("    <meta charset='utf-8'/>\n");
     res.write("  </head>\n");
     res.write("  <body>\n");
-    var savedValues = query.split("#");
-    var array = ['T', 'Z', 'Y', 'X', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'X'];
-    for (i in array) {
-        array[i] = savedValues[i];
-    }
+    var savedVals = query.split("#");
     
+    for (i in savedVals) {
+        fs.writeFileSync('values.vls', savedVals[i] + "\n");
+    }
+
     res.write("    <p>Saved values on file"+ "</p>");
 
-    fs.appendFileSync('phone.lis', fornamn + " " + efternamn + "," + adress + "\n");
-
-    let data = fs.readFileSync('phone.lis');
+    let data = fs.readFile('values.vls');
     // res.write(data);
-    let lines = data.toString().split(/\r?\n/);
-    for (l of lines) {
-        res.write("<p>" + l + "</p>");
+    for (i in data) {
+        res.write("<p>" + arrayV[i] + ":" +  i + "</p>");
     }
     res.write("  </body>\n");
     res.write("</html>\n");
 }
 
 function mainPage(res) {
-    fs.readFile("calc5000xPointY.html", function (err, data) {
+    fs.readFile("calc5000xPointY.html", function (err, calc) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
+        let data = fs.readFile('values.vls');
+        for (i in calc) {
+                        if (i.includes("var s =")) {
+                            for (int j = 0; j < 4; j++){ 
+                                i.replace("T: 0", arrayV[j]+ ": " + data[j]);
+                                }
+            }
+        }
+        res.write(calc);
         return res.end();
     });
 }
